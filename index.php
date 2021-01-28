d004a644a6dc0e05d4612583b22af6ac61347012
Patrick Dang <pdang2@mail.greenriver.edu>
2021-01-27 20:01:50 -0500

    added helpful notes explaining routes/controller page

--- index.php
+++ index.php
@@ -19,6 +19,24 @@ $f3->route('GET /', function(){
     $view = new Template();
     echo $view->render('views/home.html');
 });
+/*
+ * Creating additional routes notes
+ * Ex: Breakfast route below
+ * 1. f3 object instantiated above -> invoking a method (java - $f3. (dot)-method call - PHP $f3 -> route
+ * 2. route method takes 2 parameters (route (Always GET or POST), anonymous function (, function())
+ * 3. 'GET / - default use forward slash - additional routes we give name aka breakfast - 'GET /breakfast' - map onto this route
+ * 4. server is now looking for an actual directory called breakfast in foods dir. Goal is we only want to name a route
+ * 5. Tell server, if we want to add additional route, GO BACK TO SERVER PAGE/CONTROLLER PAGE
+ * 6. Controller page handles by using an htaccess file
+ * 7. htaccess file solves 2 problems -
+ * 7.1. No going through pages directly / only through routes
+ * 7.2. Typing in URL, we want our index/controller page to handle all request needs to go through the controller.
+ *
+ */
+//Define a breakfast route
+$f3->route('GET /breakfast', function(){
+    echo "Breakfast";
+});
 
 //Run fat free
 $f3->run();
